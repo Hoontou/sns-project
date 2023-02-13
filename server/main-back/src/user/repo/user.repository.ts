3aa5ef7a3510983f7b0d84ed3701a73e7788b318
hoontou <hoontou@gmail.com>
2023-02-14 05:59:28 +0900

    miner

--- server/main-back/src/user/repo/user.repository.ts
+++ server/main-back/src/user/repo/user.repository.ts
@@ -19,21 +19,26 @@ export class UserTable {
   async signUp(user: SignUpDto): Promise<User> {
     //근데 받아오는 user객체의 비밀번호는 암호화 돼 있어서 정확히는 Dto에 부합하지 않음.
     const newUser: User = this.db.create(user);
+    let success = true;
     try {
       await this.db.save(newUser);
       //콘솔테스트
       // const result = await this.db.save(user);
       // console.log(result);
     } catch (error) {
+      success = false;
       console.log(error.code);
       if (error.code === '23505') {
         throw new ConflictException('Existing username or eamil');
       }
       throw new InternalServerErrorException();
     } finally {
-      this.userNumsTable.createUserNums(newUser);
-    }
+      //유저생성 성공했으면 usernums테이블에 insert한다.
+      if (success == true) {
+        this.userNumsTable.createUserNums(newUser);
+      }
 
-    return newUser;
+      return newUser;
+    }
   }
 }
