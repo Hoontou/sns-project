edc44ed71cdd80f82ca29b2b5eb8ba69d544d021
hoontou <hoontou@gmail.com>
2023-02-24 19:55:29 +0900

    miner

--- server/metadata/src/database/schema.ts
+++ server/metadata/src/database/schema.ts
@@ -16,11 +16,11 @@ interface MetadataDto {
 const Metadata = mongoose.model('metadata', metadataSchema);
 
 //Dto파싱해서 document로 만들어 저장까지 해주는 함수.
-export const newMeatadata = (metadataDto: MetadataDto) => {
+export const newMeatadata = async (metadataDto: MetadataDto) => {
   metadataDto.userUuid = crypter.decrypt(metadataDto.userUuid);
   //암호화된 userUuid가 들어오니 복호화 해주고
   const newOne = new Metadata(metadataDto);
-  newOne.save();
+  await newOne.save();
   //Document만들어서 저장까지 해준다.
   return newOne;
 };
