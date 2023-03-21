8761c6de21d2f977d647bd6d86650d9fba51b974
hoontou <hoontou@gmail.com>
2023-03-22 01:50:03 +0900

    miner

--- client/src/dev-component/Post.tsx
+++ client/src/dev-component/Post.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 interface MetadataDto {
   _id: string;
-  user_id: string;
+  userId: string;
   files: string[];
 }
 
@@ -11,20 +11,21 @@ const Post = () => {
   const [postList, setPostList] = useState<Array<MetadataDto>>([]);
 
   const onClick = async () => {
-    const user_id = await axios.get('/main-back/user/hoc').then((res) => {
-      return res.data.user_id;
+    const userId = await axios.get('/main-back/user/hoc').then((res) => {
+      return res.data.userId;
     });
-    console.log(user_id);
-    axios.post('/metadata/getposts', { user_id }).then((res) => {
+    console.log(userId);
+    axios.post('/metadata/getposts', { userId }).then((res) => {
       //res.data =  MetadataDto[]
       const posts: MetadataDto[] = res.data.posts;
+      console.log(posts);
       setPostList([...postList, ...posts]);
       //기존의 postlist에다가 뒤에 가져온 posts를 붙인다.
       //나중에 mongo에서 limit걸어서 스크롤 내릴수록 계속 불러올거임.
       //지금은 메타데이터에서 포스트를 싹다 불러와줌. 한번만 클릭해
     });
   };
-  //https://snsupload.blob.core.windows.net/post_id/files[0]
+  //https://snsupload.blob.core.windows.net/postId/files[0]
   return (
     <div>
       this is post<button onClick={onClick}>getPosts</button>
@@ -32,7 +33,7 @@ const Post = () => {
         {postList.map((i) => {
           return (
             <div>
-              <h2>post_id = {i._id}</h2>
+              <h2>postId = {i._id}</h2>
               {i.files.map((j) => {
                 return (
                   <img
