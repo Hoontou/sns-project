e5597b77db691d14129ae56bb454957e688dee4c
hoontou <hoontou@gmail.com>
2023-08-21 02:08:58 +0900

    몽고디비 레플리카셋 구성, 엘라스틱 연동할차례

---
+++ db.setting/mgdb.repl.config/set.js
@@ -0,0 +1,20 @@
+rs.initiate({
+  _id: 'rs0',
+  members: [
+    {
+      _id: 0,
+      host: 'mgdb:27017',
+      priority: 2,
+    },
+    {
+      _id: 1,
+      host: 'mgdb2:27017',
+      priority: 0.5,
+    },
+    {
+      _id: 2,
+      host: 'mgdb3:27017',
+      priority: 0.5,
+    },
+  ],
+});
---
+++ db.setting/mgdb.repl.config/set.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start mongod in the background with the specified options
+mongod --replSet rs0 --bind_ip_all
+
+# Wait for 10 seconds
+sleep 10
+
+# Run mongosh with the provided script file
+mongosh /usr/src/config/set.js
